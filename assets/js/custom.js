5654a1402ffbf0d77dd90d9a2b7f231e4b419166
Adam Kachidurian <akachidurian@gmail.com>
2023-02-23 23:41:15 -0500

    Working on adding content
    Resume and projects are being added

---
+++ assets/js/custom.js
@@ -0,0 +1,43 @@
+/*
+    This script changes the content of generic.html
+    Adam Kachidurian
+    02-23-2023
+*/
+
+$(document).ready(function() {
+    $('#project-container').on('click', '.button', function() {
+        const btnText = $(this).text();
+        
+        $.getJSON("/files/PageContent.json", function(data) {
+            let contentJSON = data; 
+            console.log(data);
+            let index = 0;
+            switch (btnText) {
+                // Website
+                case 'This Site':
+                    index = 0;
+                    break;
+                // School
+                case 'Snake in C':
+                    index = 1;
+                    break;
+                case 'Java Game':
+                    index = 2;
+                    break;
+                case 'News Site':
+                    index = 3;
+                    break;
+            }
+
+            console.log(contentJSON[index].Title)
+            pageChange(contentJSON[index].Title, contentJSON[index].Description, contentJSON[index].Image, contentJSON[index].Link)   
+        });
+
+        function pageChange(title, desc, img, link) {
+            $('.proj-title').text(title);
+            $('.proj-desc').text(desc);
+            $('.proj-img').attr('src', img);
+            $('.proj-link').attr('href', link);
+        }
+    });
+});
